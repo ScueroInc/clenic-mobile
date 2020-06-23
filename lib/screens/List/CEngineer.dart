012f1cfb14de7abd049535bc3324e35138bb07bb
aduii <alexjuep@hotmail.com>
2020-06-22 22:44:16 -0500

    gitignore

--- lib/screens/List/CEngineer.dart
+++ lib/screens/List/CEngineer.dart
@@ -1,4 +1,17 @@
 class CEngineer{
   int id;
   String name;
+  String email;
+  String telephone;
+
+  CEngineer({this.id, this.name,this.email,this.telephone});
+
+  factory CEngineer.fromJson(Map<String,dynamic>json){
+    return  CEngineer(
+      id:json["id"] as int,
+      name: json["nombre"] as String,
+      email: json["email"] as String,
+      telephone: json ["telephone"] as String,
+    );
+  }
 }
